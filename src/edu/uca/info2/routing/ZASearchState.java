18ec12a428d1f058d9b8caa0ec6c1826f0990eaf
Diego Allen <diegoallen@gmail.com>
2013-08-26 14:43:09 -0400

    Agregue el area a ZASearchState. Manejo el caso de segmentos invertidos al incrementar el contador de segmentos

--- src/edu/uca/info2/routing/ZASearchState.java
+++ src/edu/uca/info2/routing/ZASearchState.java
@@ -15,12 +15,14 @@ public class ZASearchState {
 	// Para contar la cantidad de veces que se paso p/ un segmento
 	private HashMap<Segment, Integer> segmentsCounter = new HashMap<Segment, Integer>();
 
+    private Area area;
+
 	// maxima cant. de veces que se puede pasar por un segmento
 	private int maxSegmentPassThrough = 1;
 
 	public ZASearchState(MapNode node, Area area) {
 		this.node = node;
-		// List<Segment> segments = MapUtils.segmentsInArea(area);
+        this.area = area;
 		List<Segment> segments = area.segmentsInArea();
 
 		// inicializa el contador de segmentos
@@ -30,8 +32,9 @@ public class ZASearchState {
 	}
 
 	public ZASearchState(Segment segment,
-			HashMap<Segment, Integer> segmentsCounter) {
+			HashMap<Segment, Integer> segmentsCounter, Area area) {
 		this.node = segment.getTo();
+        this.area = area;
 
 		// inicializa el contador de segmentos
 		for (Map.Entry<Segment, Integer> entry : segmentsCounter.entrySet()) {
@@ -42,6 +45,10 @@ public class ZASearchState {
 		incrementSegmentCounter(segment);
 	}
 
+    public Area getArea() {
+        return area;
+    }
+
 	public MapNode getNode() {
 		return node;
 	}
@@ -51,10 +58,16 @@ public class ZASearchState {
 	}
 
 	private void incrementSegmentCounter(Segment segment) {
-		if (segmentsCounter.containsKey(segment)) {
-			int counter = segmentsCounter.get(segment) + 1;
+	    int counter;
+
+        if (segmentsCounter.containsKey(segment)) {
+			counter = segmentsCounter.get(segment) + 1;
 			segmentsCounter.put(segment, counter);
-		}
+		} else {
+            Segment invertedSegment = segment.inverted();
+            counter = segmentsCounter.get(invertedSegment) + 1;
+            segmentsCounter.put(segment, counter);
+        }
 	}
 
 	public int getMaxSegmentPassThrough() {
